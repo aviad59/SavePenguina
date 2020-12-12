c5158f93a3e759a98bbee0e0097db55b7fd45c3f
idan aviad <idanaviad10@gmail.com>
2020-12-12 19:32:31 +0200

    first commit

---
+++ Assets/graphics/waterflow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterflow : MonoBehaviour
+{
+    public TerrainLayer water;
+    float speed = 0.02f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (water.tileOffset.x % 75 == 0)
+            water.tileOffset = new Vector2(0, 0);
+
+        water.tileOffset = new Vector2(water.tileOffset.x + speed, water.tileOffset.y);
+    }
+}
